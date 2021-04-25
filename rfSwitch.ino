3b3d8bfb90cd8c4b77c72ac1539b6e5c03073c3e
ennteresan <83180724+ennteresan@users.noreply.github.com>
2021-04-25 20:47:34 +0300

    Add files via upload

---
+++ rfSwitch.ino
@@ -0,0 +1,22 @@
+int pirPin = 8;                   
+int hareket;
+int sonHareket=0;
+#include <RCSwitch.h>
+RCSwitch mySwitch = RCSwitch();
+void setup() {
+  Serial.begin(9600);
+  mySwitch.enableTransmit(10);
+  pinMode(pirPin, INPUT);   
+}
+void loop() {
+  hareket = digitalRead(pirPin);
+   if (hareket != sonHareket){
+   if(hareket == HIGH){
+mySwitch.switchOn("11111", "00010");
+   }
+  else(hareket == LOW){   
+mySwitch.switchOff("11111", "00010");
+}
+}sonHareket = hareket;
+delay(10);
+}
